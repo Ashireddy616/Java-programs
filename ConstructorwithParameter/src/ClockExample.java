ede48d537c5381536c5dead0c81afe0611d9cb41
Ashireddy616 <75776266+Ashireddy616@users.noreply.github.com>
2020-12-16 05:35:46 -0800

    Constructor With parameter

---
+++ ConstructorwithParameter/src/ClockExample.java
@@ -0,0 +1,19 @@
+
+public class ClockExample {
+	String brand;
+	String color;
+	int radious;
+	ClockExample(String inBrand,String inColor,int inRadious)
+	{
+		brand = inBrand;
+		color = inColor;
+		radious = inRadious;
+	}
+	ClockExample()
+	{
+		String brand;
+		String color;
+		int Radious;
+	}
+	
+}
---
+++ ConstructorwithParameter/src/ClockExampleTester.java
@@ -0,0 +1,20 @@
+
+public class ClockExampleTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClockExample A = new ClockExample("Black","Sonanta",25);
+		System.out.println("Color is:"+ A.color);
+		System.out.println("Brand is:" +A.brand);
+		System.out.println("radious is" +A.radious);
+		
+		System.out.println("===============");
+		
+		ClockExample A1 = new ClockExample();
+		
+		System.out.println("Brand is:" +A1.brand);
+		
+
+	}
+
+}
